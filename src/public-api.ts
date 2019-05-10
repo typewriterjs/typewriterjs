15108104ce8b709a2db34f907cdbba083e1ee628
codemile <nick@reactgular.com>
2019-05-10 14:12:10 -0400

    tests compiling

--- src/public-api.ts
+++ src/public-api.ts
@@ -1,3 +1,1 @@
 export * from './core';
-export {RenderBufferComponent} from './angular/render-buffer/render-buffer.component';
-export {RgAnimatedTypingModule} from './angular/rg-animated-typing.module';
